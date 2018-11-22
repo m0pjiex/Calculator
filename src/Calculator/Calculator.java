dc68698cf0ba1d2552dca653d176599faab4f8f3
m0pjiex <trolologhost3@gmail.com>
2018-11-23 00:58:00 +0700

    Vichislrnia

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,12 @@
+package Calculator;
+
+public class Calculator {
+    double Sum(double num1, double num2){
+        double sum = num1 +num2;
+        return sum;
+    }
+    double Del(double num1, double num2){
+        double sum = num1 / num2;
+        return sum;
+    }
+}
--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -2,6 +2,10 @@ package Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Calculator calculator = new Calculator();
+        double x = 5;
+        double y = 5;
+        System.out.println(calculator.Sum(x, y));
+        System.out.println(calculator.Del(x, y));
     }
 }
